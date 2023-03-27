e6eb1c4e03da0fb1364a29c332d0348455a9f849
sion99 <kmicety1@gmail.com>
2023-03-27 21:29:53 +0900

    ft_split done

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,53 +3,101 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sishin <sishin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: sishin <sishin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:56:12 by sishin            #+#    #+#             */
-/*   Updated: 2023/03/27 19:49:41 by sishin           ###   ########.fr       */
+/*   Updated: 2023/03/27 21:29:23 by sishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_splittednum(char const *s, char c)
+int	ft_split_num(char const *str, char c)
 {
-	size_t	i;
-	size_t	len;
-	int		snum;
+	int	i;
+	int	total;
 
 	i = 0;
-	len = ft_strlen(s);
-	while (i < len)
+	total = 0;
+	while (str[i])
 	{
-		if(s[i] == c)
-			snum++;
-		i++;
+		while (str[i] == c && str[i])
+			i++;
+		if (str[i] != c && str[i] != '\0')
+			total++;
+		while ((str[i] != c && str[i] != '\0') && str[i])
+			i++;
 	}
-	return (snum + 1);
+	return (total);
 }
 
-char	**free2d(char **s, int range)
+char	*ft_strncpy(char *dest, char *src, int n)
+{
+	int	count;
+
+	count = 0;
+	while (src[count] && count < n)
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
+
+char	**free2d(char **splitted, int count)
 {
 	int	i;
 
 	i = 0;
-	while (i < range)
+	while (i < count)
 	{
-		free(s[i]);
+		free(splitted[i]);
 		i++;
 	}
-	free(s);
-	return (0);
+	free(splitted);
+	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_allocate(char **splitted, char const *str, char c, int s_num)
 {
-	int		snum;
-	char	**splitted;
+	int	count;
+	int	i;
+	int	last;
 
-	snum = ft_splittednum(s, c);
-	splitted = (char **)malloc(sizeof(char *) * (snum + 1));
+	count = 0;
+	i = 0;
+	while (count < s_num && str[i])
+	{
+		while (str[i] == c && str[i])
+			i++;
+		last = i;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+		splitted[count] = (char *)malloc(sizeof(char) * (i - last + 1));
+		if (!splitted[count])
+			return (free2d(splitted, count));
+		ft_strncpy(splitted[count], (char *)&str[last], i - last);
+		last = i;
+		count++;
+	}
+	return (splitted);
+}
+
+char	**ft_split(char const *str, char c)
+{
+	char	**splitted;
+	int		s_num;
+	int		len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	s_num = ft_split_num(str, c);
+	splitted = (char **)malloc(sizeof(char *) * (s_num + 1));
 	if (!splitted)
-		return(0);
+		return (NULL);
+	splitted = ft_allocate(splitted, str, c, s_num);
+	splitted[s_num] = NULL;
+	return (splitted);
 }
